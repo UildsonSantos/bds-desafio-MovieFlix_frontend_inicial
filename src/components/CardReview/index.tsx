533baa1dfca796046c5879c6eaa309dd644125ea
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-11-17 23:15:12 -0300

    Implemented feature to get movie reviews list

--- src/components/CardReview/index.tsx
+++ src/components/CardReview/index.tsx
@@ -1,20 +1,19 @@
+import { Review } from 'types/Review';
 import Star from 'assets/images/star.png';
 
 import './styles.css';
 
 type Props = {
-  username: string;
-  comment: string;
+  review: Review;
 };
-
-const CardReview = ({ username, comment }: Props) => {
+const CardReview = ({ review }: Props) => {
   return (
-    <div className="card-review-container">
+    <div className="card-review-container" key={review.id}>
       <div className="card-review-title">
         <img src={Star} alt="star" />
-        <h6>{username}</h6>
+        <h6>{review.user.name}</h6>
       </div>
-      <p>{comment}</p>
+      <p>{review.text} </p>
     </div>
   );
 };
--- src/pages/MovieDetails/index.tsx
+++ src/pages/MovieDetails/index.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AxiosRequestConfig } from 'axios';
+
 import ButtonIcon from 'components/ButtonIcon';
 import CardReview from 'components/CardReview';
+import { requestBackend } from 'util/requests';
+import { Review } from 'types/Review';
 
 import './styles.css';
 
+type UrlParams = {
+  movieId: string;
+};
+
 const MovieDetails = () => {
+  const { movieId } = useParams<UrlParams>();
+  const [movieReviews, setMovieReviews] = useState<Review[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const params: AxiosRequestConfig = {
+      url: `/movies/${movieId}/reviews`,
+      withCredentials: true,
+    };
+    requestBackend(params)
+      .then((response) => {
+        setMovieReviews(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [movieId]);
+
   return (
     <>
       <div className="movie-details-container">
@@ -19,19 +48,15 @@ const MovieDetails = () => {
         </div>
 
         <div className="movie-details-reviews">
-          <CardReview username="Bob" comment="Melhor filme do ano" />
-          <CardReview
-            username="Ana"
-            comment="Gostei muito do final do filme, quando será que vai sair a continuação"
-          />
-          <CardReview
-            username="Maria"
-            comment="Hahahah ...também gostei, o próximo será uma loucuraaa"
-          />
-          <CardReview
-            username="Alex"
-            comment="Esse merece ser uma serie ...pra gente maratonar heheheh"
-          />
+          {isLoading ? (
+            <h3>Carregando...</h3>
+          ) : movieReviews && movieReviews.length > 0 ? (
+            movieReviews?.map((ele) => (
+              <CardReview key={ele.id} review={ele}></CardReview>
+            ))
+          ) : (
+            <h3>Este filme não possui comentários</h3>
+          )}
         </div>
       </div>
     </>
