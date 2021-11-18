e7e81e149361e01f57f43eedd52bc814b7eac399
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-11-17 23:29:07 -0300

    Implemented feature for member profile to post reviews

--- src/pages/MovieDetails/index.tsx
+++ src/pages/MovieDetails/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { AxiosRequestConfig } from 'axios';
 
 import ButtonIcon from 'components/ButtonIcon';
 import CardReview from 'components/CardReview';
 import { requestBackend } from 'util/requests';
+import { hasAnyRoles } from 'util/auth';
 import { Review } from 'types/Review';
 
 import './styles.css';
@@ -13,10 +15,31 @@ type UrlParams = {
   movieId: string;
 };
 
+type FormData = {
+  text: string;
+  movieId: number;
+};
+
 const MovieDetails = () => {
   const { movieId } = useParams<UrlParams>();
   const [movieReviews, setMovieReviews] = useState<Review[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    const params: AxiosRequestConfig = {
+      method: 'POST',
+      url: `/reviews`,
+      withCredentials: true,
+      data: {
+        text: formData.text,
+        movieId: movieId,
+      },
+    };
+    requestBackend(params).then((response) => {
+      setMovieReviews((movieReviews) => [...movieReviews, response.data]);
+    });
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -36,16 +59,21 @@ const MovieDetails = () => {
   return (
     <>
       <div className="movie-details-container">
-        <h1>Tela detalhes do filme id: 1</h1>
-        <div className="base-card card-rating-container">
-          <form>
-            <input placeholder="Deixe sua avaliação aqui" />
+      <h1>Tela detalhes do filme id: {movieId}</h1>
+        {hasAnyRoles(['ROLE_MEMBER']) && (
+          <div className="base-card card-rating-container">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                {...register('text')}
+                placeholder="Deixe sua avaliação aqui"
+              />
 
-            <div>
-              <ButtonIcon text="SALVAR AVALIAÇÃO" />
-            </div>
-          </form>
-        </div>
+              <div>
+                <ButtonIcon text="SALVAR AVALIAÇÃO" />
+              </div>
+            </form>
+          </div>
+        ) }
 
         <div className="movie-details-reviews">
           {isLoading ? (
